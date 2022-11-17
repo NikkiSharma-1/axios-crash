63ab5b536204cdb48b1550fa6c95d613ce84f103
NikkiSharma-1 <114333988+NikkiSharma-1@users.noreply.github.com>
2022-11-17 17:35:08 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -3,22 +3,31 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const errorController=require('./error');
+const errorController = require('./controllers/error');
+const db = require('./util/database');
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+
+db.execute('SELECT * FROM products')
+  .then(result => {
+    console.log(result[0], result[1]);
+  })
+  .catch(err => {
+    console.log(err);
+  });
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(errorController.err);
+app.use(errorController.get404);
 
 app.listen(3000);
